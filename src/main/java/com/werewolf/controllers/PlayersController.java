5ea7f007bdb8ab11dc01232671d666edc6313a2a
QiannanXu <qnxu@thoughtworks.com>
2017-02-22 12:01:12 +0800

    Add player controller to support start game

--- src/main/java/com/werewolf/controllers/PlayersController.java
+++ src/main/java/com/werewolf/controllers/PlayersController.java
@@ -1,4 +1,18 @@
 package com.werewolf.controllers;
 
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PlayersController {
+
+    @RequestMapping(value = "/players/{id}", method = RequestMethod.PUT)
+    public String readyToGame(@PathVariable Integer id, @RequestBody String body ) {
+        Boolean isReady = (Boolean) new JSONObject(body).get("isReady");
+        return isReady.toString();
+    }
 }
