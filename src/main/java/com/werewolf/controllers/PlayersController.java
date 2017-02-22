49cf740884310113601d596f54676f1d5c5784b3
QiannanXu <qnxu@thoughtworks.com>
2017-02-22 14:24:35 +0800

    Set player ready status

--- src/main/java/com/werewolf/controllers/PlayersController.java
+++ src/main/java/com/werewolf/controllers/PlayersController.java
@@ -1,6 +1,10 @@
 package com.werewolf.controllers;
 
+import com.werewolf.models.Game;
+import com.werewolf.models.Player;
+import java.util.Optional;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PlayersController {
 
+    @Autowired
+    private Game game;
+
     @RequestMapping(value = "/players/{id}", method = RequestMethod.PUT)
-    public String readyToGame(@PathVariable Integer id, @RequestBody String body ) {
-        Boolean isReady = (Boolean) new JSONObject(body).get("isReady");
-        return isReady.toString();
+    public void readyToGame(@PathVariable Integer id, @RequestBody String body ) {
+        Boolean isReady = new JSONObject(body).getBoolean("isReady");
+        Optional<Player> player = game.getPlayerById(id);
+
+        if(player.isPresent()) {
+            player.get().setReady(isReady);
+        }
     }
 }
--- src/main/java/com/werewolf/models/Game.java
+++ src/main/java/com/werewolf/models/Game.java
@@ -1,10 +1,26 @@
 package com.werewolf.models;
 
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Game {
 
-    public Game() {};
+    private Map<Integer, Player> playerMap;
+
+    public Game(Map<Integer, Player> playerMap) {
+        this.playerMap = playerMap;
+    }
 
     public Game(GameConfiguration gameConfiguration) {
 
+    }
+
+    public Optional<Player> getPlayerById(Integer id) {
+        if(playerMap.containsKey(id)) {
+            return Optional.of(playerMap.get(id));
+        }
+        return Optional.empty();
     }
 }
