9553a4050bb5222f2d019c7c199a518b5fb21872
Chao Tuo <ctuo@thoughtworks.com>
2017-02-22 15:01:20 +0800

    Add process status in judge class.

--- src/main/java/com/werewolf/models/Judge.java
+++ src/main/java/com/werewolf/models/Judge.java
@@ -1,55 +1,70 @@
 package com.werewolf.models;
 
+import java.util.HashMap;
+
 /**
  * Created by ctuo on 2/22/17.
  */
 public class Judge {
-    public Judge(GameStatus gameStatus) {
+  public Judge(GameStatus initGameStatus) {
+    this.currentGameStatus = initGameStatus;
+    processStatus = POCESS_STATUS.NIGHT_START;
+  }
 
+
+  private enum POCESS_STATUS {
+    NIGHT_START("天黑了,请大家闭眼"),
+    WOLF_APPEAR("狼人请睁眼"),
+    WOLF_KILL("狼人请杀人,给我一个座位号"),
+    WOLF_UNIFY_OPINION("狼人请统一意见"),
+    WOLF_VANISH("狼人请闭眼"),
+    DAY_START("天亮了,请大家睁眼");
+
+    private String message;
+
+    POCESS_STATUS (String message) {
+      this.message = message;
     }
 
-    public String nightComing() {
-        return "天黑了,请大家闭眼";
+    public String getMessage() {
+      return message;
     }
+  }
 
-    public String dayComing() {
-        return "天亮了,请大家睁眼";
-    }
+  private POCESS_STATUS processStatus;
 
-    public String callWolftoAppear() {
-        return "狼人请睁眼";
-    }
+  private GameStatus currentGameStatus;
 
-    public String callWolftoKill() {
-        return "狼人请杀人,给我一个座位号";
-    }
+  public String start() {
+    return POCESS_STATUS.NIGHT_START.getMessage();
+  }
 
-    public String unifyOpinion() {
-        return "狼人请统一意见";
+  public String next(GameStatus currentGameStatus) {
+    this.currentGameStatus = currentGameStatus;
+    if(processStatus == POCESS_STATUS.NIGHT_START) {
+      processStatus = POCESS_STATUS.WOLF_APPEAR;
+      return POCESS_STATUS.WOLF_APPEAR.getMessage();
     }
+  }
 
-    public String callWolftoVanish() {
-        return "狼人请闭眼";
-    }
+  public String callWitch() {
+      return "";
+  }
 
-    public String callWitch() {
-        return "";
-    }
+  public String callProphet() {
+      return "";
+  }
 
-    public String callProphet() {
-        return "";
-    }
+  public String callHunter() {
+      return "";
+  }
 
-    public String callHunter() {
-        return "";
-    }
+  public String callSheriff() {
+      return "";
+  }
 
-    public String callSheriff() {
-        return "";
-    }
-
-    public Boolean isEnd() {
-        return false;
-    }
+  public Boolean isEnd() {
+    return false;
+  }
 
 }
