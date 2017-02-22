490ee967c37a251ee642270d5e94281c2ec6da1f
Chao Tuo <ctuo@thoughtworks.com>
2017-02-22 16:39:12 +0800

    Refactor judge status machine.

--- src/main/java/com/werewolf/models/Judge.java
+++ src/main/java/com/werewolf/models/Judge.java
@@ -1,71 +1,26 @@
 package com.werewolf.models;
 
-import java.util.HashMap;
-
 /**
  * Created by ctuo on 2/22/17.
  */
 public class Judge {
   public Judge(GameStatus initGameStatus) {
     this.currentGameStatus = initGameStatus;
-    processStatus = POCESS_STATUS.NIGHT_START;
+    processStatus = new ProcessStatus();
   }
 
-  private enum POCESS_STATUS {
-    NIGHT_START("天黑了,请大家闭眼"),
-    WOLF_APPEAR("狼人请睁眼"),
-    WOLF_KILL("狼人请杀人,给我一个座位号"),
-    WOLF_UNIFY_OPINION("狼人请统一意见"),
-    WOLF_VANISH("狼人请闭眼"),
-    DAY_START("天亮了,请大家睁眼");
-
-    private String message;
-
-    POCESS_STATUS (String message) {
-      this.message = message;
-    }
-
-    public String getMessage() {
-      return message;
-    }
-  }
-
-  private POCESS_STATUS processStatus;
   private GameStatus currentGameStatus;
+  private ProcessStatus processStatus;
 
   public String start() {
-    return POCESS_STATUS.NIGHT_START.getMessage();
+    processStatus.setInitStatus();
+    return processStatus.getMessage();
   }
 
   public String next(GameStatus currentGameStatus) {
     this.currentGameStatus = currentGameStatus;
-    if(processStatus == POCESS_STATUS.NIGHT_START) {
-      processStatus = POCESS_STATUS.WOLF_APPEAR;
-      return POCESS_STATUS.WOLF_APPEAR.getMessage();
-    }
-
-    if(processStatus == POCESS_STATUS.WOLF_APPEAR) {
-      processStatus = POCESS_STATUS.WOLF_KILL;
-      return POCESS_STATUS.WOLF_KILL.getMessage();
-    }
-
-    if(processStatus == POCESS_STATUS.WOLF_KILL) {
-      if(currentGameStatus.getWolfKillNO() > 0) {
-        processStatus = POCESS_STATUS.WOLF_UNIFY_OPINION;
-        return POCESS_STATUS.WOLF_UNIFY_OPINION.getMessage();
-      }
-      else {
-        processStatus = POCESS_STATUS.WOLF_VANISH;
-        return POCESS_STATUS.WOLF_VANISH.getMessage();
-      }
-    }
-
-    if(processStatus == POCESS_STATUS.WOLF_VANISH) {
-      processStatus = POCESS_STATUS.DAY_START;
-      return POCESS_STATUS.DAY_START.getMessage();
-    }
-
-    return "";
+    processStatus.goNextStatus(processStatus.getCurrentStatus());
+    return processStatus.getMessage();
   }
 
   public void updateGameStatus(GameStatus gameStatue) {
