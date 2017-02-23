b3bdc857e4cd98ef5b885e49da64ad58a4d1d433
Chao Tuo <ctuo@thoughtworks.com>
2017-02-23 10:12:37 +0800

    Change implementation of game state.

--- src/main/java/com/werewolf/models/GameState.java
+++ src/main/java/com/werewolf/models/GameState.java
@@ -6,16 +6,16 @@ import java.util.HashMap;
 
 public class GameState {
 
-  public GameState(int roomNO, int totalPlayerCount, HashMap<Integer, Player> playerMap) {
-    this.roomNo = roomNO;
+  public GameState(int roomID, int totalPlayerCount, ArrayList<Player> playerList) {
+    this.roomNo = roomID;
     this.totalPlayerCount = totalPlayerCount;
-    this.playerMap = playerMap;
+    this.playerList = playerList;
   }
 
   private int roomNo;
   private int totalPlayerCount;
-  private HashMap<Integer, Player> playerMap;
-  private int wolfKillNO;
+  private ArrayList<Player> playerList;
+  private int wolfKillID;
 
   public int getRoomNo() {
     return roomNo;
@@ -25,18 +25,43 @@ public class GameState {
     return totalPlayerCount;
   }
 
-  public int getWolfKillNO() {
-    return wolfKillNO;
+  public HashMap<String, Integer> getAlivePlayerInfo() {
+    HashMap<String, Integer> alivePlayerInfo  = new HashMap<String, Integer>();
+    alivePlayerInfo.put("GOD",0);
+    alivePlayerInfo.put("WEREWOLF",0);
+    alivePlayerInfo.put("VILLAGER",0);
+
+    int tempCount = 0;
+    for(Player playerTemp : playerList) {
+      if(playerTemp.isAlive()) {
+        switch(playerTemp.getRole().getType()) {
+          case (0):
+            tempCount = alivePlayerInfo.get("GOD");
+            tempCount ++;
+            alivePlayerInfo.put("GOD", tempCount);
+            break;
+          case (1):
+            tempCount = alivePlayerInfo.get("WEREWOLF");
+            tempCount ++;
+            alivePlayerInfo.put("WEREWOLF", tempCount);
+            break;
+          case (2):
+            tempCount = alivePlayerInfo.get("VILLAGER");
+            tempCount ++;
+            alivePlayerInfo.put("VILLAGER", tempCount);
+            break;
+        }
+      }
+    }
+    return alivePlayerInfo;
   }
 
-  public void setWolfKillNO(int killNO) {
-    wolfKillNO = killNO;
+  public int getWolfKillID() {
+    return wolfKillID;
   }
 
-  public boolean isPlayerAlive(int setNO) {
-    return playerMap.get(setNO).isAlive();
+  public void setWolfKillID(int killID) {
+    wolfKillID = killID;
   }
-
-
 
 }
--- src/main/java/com/werewolf/models/Judge.java
+++ src/main/java/com/werewolf/models/Judge.java
@@ -6,50 +6,51 @@ import java.util.HashMap;
 public class Judge {
   public Judge(GameState initGameState) {
     this.currentGameState = initGameState;
-    processRelation = new ProcessRelation();
+    stateRelation = new StateRelation();
 
-    PROCESS_DATA_MAP = new HashMap<String, String>();
-    PROCESS_DATA_MAP.put("NIGHT_START", "天黑了,请大家闭眼");
-    PROCESS_DATA_MAP.put("WOLF_APPEAR", "狼人请睁眼");
-    PROCESS_DATA_MAP.put("WOLF_KILL", "狼人请杀人,给我一个座位号");
-    PROCESS_DATA_MAP.put("WOLF_UNIFY_OPINION", "狼人请统一意见");
-    PROCESS_DATA_MAP.put("WOLF_VANISH", "狼人请闭眼");
-    PROCESS_DATA_MAP.put("DAY_START", "天亮了,请大家睁眼");
+    STATE_DATA_MAP = new HashMap<String, String>();
+    STATE_DATA_MAP.put("NIGHT_START", "天黑了,请大家闭眼");
+    STATE_DATA_MAP.put("WOLF_APPEAR", "狼人请睁眼");
+    STATE_DATA_MAP.put("WOLF_KILL", "狼人请杀人,给我一个座位号");
+    STATE_DATA_MAP.put("WOLF_UNIFY_OPINION", "狼人请统一意见");
+    STATE_DATA_MAP.put("WOLF_VANISH", "狼人请闭眼");
+    STATE_DATA_MAP.put("WITCH_APPEAR", "女巫请睁眼");
+    STATE_DATA_MAP.put("WITCH_VANISH", "女巫请闭眼");
+    STATE_DATA_MAP.put("PROPHET_APPEAR", "预言家请睁眼");
+    STATE_DATA_MAP.put("PROPHET_VANISH", "预言家请闭眼");
+    STATE_DATA_MAP.put("HUNTER_APPEAR", "猎人请睁眼");
+    STATE_DATA_MAP.put("HUNTER_VANISH", "猎人请闭眼");
+    STATE_DATA_MAP.put("DAY_START", "天亮了,请大家睁眼");
+    STATE_DATA_MAP.put("APPLY_SHERIFF", "请大家考虑,是否要上警");
+    STATE_DATA_MAP.put("VOTE_FOR_APPLY_SHERIFF", "要上警的请投票,5,4,3,2,1");
+    STATE_DATA_MAP.put("SHERIFF_CANDIDATE_RESULT", "上警的人有");
+    STATE_DATA_MAP.put("SHERIFF_CANDIDATE_SPEECH", "请上警的人从小号开始发言");
+    STATE_DATA_MAP.put("VOTE_FOR_SHERIFF", "请大家选举警长,5,4,3,2,1");
+    STATE_DATA_MAP.put("SHERIFF_RESULT", "警长是");
+    STATE_DATA_MAP.put("NIGHT_RESULT", "昨天晚上死的人是");
+    STATE_DATA_MAP.put("DAY_SPEECH", "请警长组织发言");
+    STATE_DATA_MAP.put("DAY_RESULT", "被投死的是");
+    STATE_DATA_MAP.put("DEATH_SPEECH", "请留遗言");
+
   }
 
-  private HashMap<String, String> PROCESS_DATA_MAP;
+  private final HashMap<String, String> STATE_DATA_MAP;
   private GameState currentGameState;
-  private ProcessRelation processRelation;
+  private StateRelation stateRelation;
 
   public String start() {
-    processRelation.setInitState();
-    return PROCESS_DATA_MAP.get(processRelation.getCurrentState());
+    stateRelation.setInitState();
+    return STATE_DATA_MAP.get(stateRelation.getCurrentState());
   }
 
   public String next(GameState currentGameState) {
     this.currentGameState = currentGameState;
-    processRelation.goNextState(processRelation.getCurrentState());
-    return PROCESS_DATA_MAP.get(processRelation.getCurrentState());
+    stateRelation.goNextState(stateRelation.getCurrentState());
+    return STATE_DATA_MAP.get(stateRelation.getCurrentState());
   }
 
   public void updateGameState(GameState gameStatue) {
     currentGameState = gameStatue;
-  }
-
-  public String callWitch() {
-      return "";
-  }
-
-  public String callProphet() {
-      return "";
-  }
-
-  public String callHunter() {
-      return "";
-  }
-
-  public String callSheriff() {
-      return "";
   }
 
   public Boolean isEnd() {
