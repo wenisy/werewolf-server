da87947583b7554accb666c4c2907d9a67c83aa1
QiannanXu <qnxu@thoughtworks.com>
2017-02-23 10:12:23 +0800

    Shuffle role in queue and fix bug during debugging with frontend

--- src/main/java/com/werewolf/models/Game.java
+++ src/main/java/com/werewolf/models/Game.java
@@ -7,7 +7,7 @@ public class Game {
 
     private Map<String, Player> playerMap;
     private String gameId;
-    private Queue<RoleType> playerQueue = new LinkedList<>();
+    private LinkedList<RoleType> playerQueue = new LinkedList<>();
 
     public Game(GameConfiguration gameConfiguration) {
         char[] digitals = "0123456789".toCharArray();
@@ -23,14 +23,13 @@ public class Game {
     }
 
     private void initPlayerQueue(GameConfiguration gameConfiguration) {
-        IntStream.range(0, gameConfiguration.getWolf()).forEach(i -> playerQueue.offer(RoleType.WEREWOLF));
-        IntStream.range(0, gameConfiguration.getVillager()).forEach(i -> playerQueue.offer(RoleType.VILLAGER));
+        IntStream.range(0, gameConfiguration.getWolf()).forEach(i -> playerQueue.add(RoleType.WEREWOLF));
+        IntStream.range(0, gameConfiguration.getVillager()).forEach(i -> playerQueue.add(RoleType.VILLAGER));
+        IntStream.range(0, gameConfiguration.getHunter()).forEach(i -> playerQueue.add(RoleType.HUNTER));
+        IntStream.range(0, gameConfiguration.getProphet()).forEach(i -> playerQueue.add(RoleType.PROPHET));
+        IntStream.range(0, gameConfiguration.getWitch()).forEach(i -> playerQueue.add(RoleType.WITCH));
 
-        IntStream.range(0, gameConfiguration.getHunter()).forEach(i -> playerQueue.offer(RoleType.HUNTER));
-        IntStream.range(0, gameConfiguration.getProphet()).forEach(i -> playerQueue.offer(RoleType.PROPHET));
-        IntStream.range(0, gameConfiguration.getWitch()).forEach(i -> playerQueue.offer(RoleType.WITCH));
-
-        //TODO 对queue进行随机排序
+        Collections.shuffle(playerQueue);
     }
 
     public Optional<Player> getPlayerById(String sessionId) {
